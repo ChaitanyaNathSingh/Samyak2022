7897876be6f24f29f0b341ee5b32709d44019e3d
Sri Hasan Prakash <hasanchadaram888@gmail.com>
2022-10-21 00:06:09 +0530

    GoSamyak

--- home/models.py
+++ home/models.py
@@ -9,7 +9,7 @@ from django.contrib.auth.models import User
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     phone = models.CharField(max_length=10, default=999999999)
-    college_name = models.CharField(max_length=30)
+    college_name = models.CharField(max_length=100)
     branch = models.CharField(max_length=20)
     year_of_study = models.TextField(max_length=15, default=1)
     gender = models.CharField(max_length=6)
