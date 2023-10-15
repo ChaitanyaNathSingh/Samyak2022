e4bcce9a031d0fee6e6c38b03f4d8b1fa692ddf4
Baba Afrid <babaafrid7@gmail.com>
2023-10-16 03:55:01 +0530

    heyy

--- backend/urls.py
+++ backend/urls.py
@@ -31,6 +31,7 @@ router = routers.DefaultRouter()
 # router.register(r'payments', views.PaymentsViewSet, basename='payments')
 router.register(r'events', views.EventsViewSet, basename='event')
 router.register(r'teams', views.TeamViewSet, basename='team')
+router.register(r'registerevent', views.RegisterEventViewSet, basename='registerevent')
 
 
 urlpatterns = [
